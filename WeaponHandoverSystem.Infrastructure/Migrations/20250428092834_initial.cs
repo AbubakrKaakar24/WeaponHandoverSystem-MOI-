975c9aa8606b5be29af12e7e753b4a052b545ea5
Abubakr Kaakar <abubakr.kaakar.2017@gmail.com>
2025-04-28 14:00:55 +0430

    DBContext Added

---
+++ WeaponHandoverSystem.Infrastructure/Migrations/20250428092834_initial.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeaponHandoverSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Officers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Base = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Moeenyat = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Directorate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Administration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BadgeNo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Officers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gate = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeaponHandovers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OutDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OfficerBadgeNo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeaponHandovers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weapons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OutDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OfficerBadgeNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OfficerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Weapons_Officers_OfficerId",
+                        column: x => x.OfficerId,
+                        principalTable: "Officers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weapons_OfficerId",
+                table: "Weapons",
+                column: "OfficerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "WeaponHandovers");
+
+            migrationBuilder.DropTable(
+                name: "Weapons");
+
+            migrationBuilder.DropTable(
+                name: "Officers");
+        }
+    }
+}
